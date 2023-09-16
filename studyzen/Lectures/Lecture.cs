9d586cd74a0e6d2c733666c7b169099fac0e9cd1
vestacic <vesta.cic@gmail.com>
2023-09-17 00:26:30 +0300

    update lecture model

--- studyzen/Lectures/Lecture.cs
+++ studyzen/Lectures/Lecture.cs
@@ -1,4 +1,5 @@
 using StudyZen.Common;
+using StudyZen.Utils;
 
 namespace Studyzen.Lectures;
 
@@ -7,6 +8,9 @@ public sealed class Lecture : BaseEntity
     public int CourseId { get; }
     public string Name { get; set; }
     public string? Content { get; set; }
+    public InstanceCreatedStamp CreatedBy { get; init; }
+    public InstanceUpdatedStamp UpdatedBy { get; set; }
+
     public List<BaseEntity>? FlashCardSets;
 
     public Lecture(int courseId, string name, string? content) : base(default)
@@ -14,5 +18,7 @@ public sealed class Lecture : BaseEntity
         CourseId = courseId;
         Name = name;
         Content = content;
+        CreatedBy = new InstanceCreatedStamp();
+        UpdatedBy = new InstanceUpdatedStamp();
     }
 }
