2ac2176781499653fe35ec67cc9c92e289501555
vestacic <vesta.cic@gmail.com>
2023-09-17 13:01:35 +0300

    update action stamp

--- studyzen/Utils/UserActionStamp.cs
+++ studyzen/Utils/UserActionStamp.cs
@@ -1,10 +1,10 @@
 namespace StudyZen.Utils;
 
-public record InstanceCreatedStamp
+public record UserActionStamp
 {
     public string User { get; init; }
     public DateTime Timestamp { get; init; }
-    public InstanceCreatedStamp(string user = "anonymous")
+    public UserActionStamp(string user = "anonymous")
     {
         User = user;
         Timestamp = DateTime.Now;
