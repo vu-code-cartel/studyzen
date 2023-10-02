a6f79edd003575c4951325402cccd88197853a65
vestacic <vesta.cic@gmail.com>
2023-10-02 23:20:45 +0300

    update dependencies

--- server/src/Application/DependencyInjection.cs
+++ server/src/Application/DependencyInjection.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using StudyZen.Application.Dtos;
 using StudyZen.Application.Services;
+using StudyZen.Application.Validators;
+using StudyZen.Domain.Entities;
 
 namespace StudyZen.Application;
 
@@ -11,6 +15,14 @@ public static class DependencyInjection
         services.AddScoped<ILectureService, LectureService>();
         services.AddScoped<IFlashcardService, FlashcardService>();
         services.AddScoped<IFlashcardSetService, FlashcardSetService>();
+        services.AddScoped<IValidator<CreateCourseDto>, CreateCourseRequestValidator>();
+        services.AddScoped<IValidator<UpdateCourseDto>, UpdateCourseRequestValidator>();
+        services.AddScoped<IValidator<CreateLectureDto>, CreateLectureRequestValidator>();
+        services.AddScoped<IValidator<UpdateLectureDto>, UpdateLectureRequestValidator>();
+        services.AddScoped<IValidator<CreateFlashcardDto>, CreateFlashcardRequestValidator>();
+        services.AddScoped<IValidator<UpdateFlashcardDto>, UpdateFlashcardRequestValidator>();
+        services.AddScoped<IValidator<CreateFlashcardDto>, CreateFlashcardRequestValidator>();
+        services.AddScoped<IValidator<UpdateFlashcardSetDto>, UpdateFlashcardSetRequestValidator>();
 
         return services;
     }
