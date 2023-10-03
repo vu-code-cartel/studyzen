19f25311f841ce1536c9213948664b9409ef73be
vestacic <vesta.cic@gmail.com>
2023-10-03 23:05:38 +0300

    create validationhandler

---
+++ server/src/Application/Validation/ValidationHandler.cs
@@ -0,0 +1,22 @@
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using StudyZen.Application.Dtos;
+
+namespace StudyZen.Application.Validation;
+
+public class ValidationHandler
+{
+    private readonly IServiceProvider _serviceProvider;
+    public ValidationHandler(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+    public void Validate<T>(T instance)
+    {
+        var validator = _serviceProvider.GetService<IValidator<T>>();
+        if (validator is not null)
+        {
+            validator.ValidateAndThrow(instance);
+        }
+    }
+}
