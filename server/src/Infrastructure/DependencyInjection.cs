4cf0d7247ada6f0d36b2baf013199aa83ef4d534
vestacic <vesta.cic@gmail.com>
2023-10-08 23:11:30 +0300

    add secrets for database

--- server/src/Infrastructure/DependencyInjection.cs
+++ server/src/Infrastructure/DependencyInjection.cs
@@ -2,13 +2,18 @@
 using StudyZen.Application.Repositories;
 using StudyZen.Infrastructure.Repositories;
 using StudyZen.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using StudyZen.Infrastructure.Persistence;
+using Microsoft.Extensions.Configuration;
 
 namespace StudyZen.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration["DATABASE_CONNECTION_STRING"]));
+
         services.AddScoped<ICourseRepository, CourseRepository>();
         services.AddScoped<ILectureRepository, LectureRepository>();
         services.AddScoped<IFlashcardRepository, FlashcardRepository>();
