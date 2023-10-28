d4e22b2168d8a4f3f33d436a91901840d544db5f
vestacic <vesta.cic@gmail.com>
2023-10-28 15:03:58 +0300

    update course service tests

--- server/tests/Application.UnitTests/services/CourseServiceTests.cs
+++ server/tests/Application.UnitTests/services/CourseServiceTests.cs
@@ -1,10 +1,12 @@
 using Moq;
+using StudyZen.Application.Exceptions;
 using StudyZen.Application.Services;
 using StudyZen.Application.Repositories;
 using StudyZen.Application.Dtos;
 using StudyZen.Application.Validation;
 using StudyZen.Domain.Entities;
 using FluentValidation;
+using System.Linq.Expressions;
 
 namespace StudyZen.tests.Application.UnitTests;
 
@@ -68,7 +70,7 @@ public class CourseServiceTests
         int courseId = 1;
         var course = new Course("name", "description");
 
-        _unitOfWorkMock.Setup(u => u.Courses.GetById(courseId)).ReturnsAsync(course);
+        _unitOfWorkMock.Setup(u => u.Courses.GetByIdChecked(courseId)).ReturnsAsync(course);
 
         var result = await _courseService.GetCourseById(courseId);
 
@@ -77,44 +79,51 @@ public class CourseServiceTests
     }
 
     [Test]
-    public async Task GetCourseById_CourseDoesNotExist_ReturnsNull()
+    public void GetCourseById_CourseDoesNotExist_ThrowsInstanceNotFoundException()
     {
         int courseId = 1;
 
-        _unitOfWorkMock.Setup(u => u.Courses.GetById(courseId)).ReturnsAsync(default(Course));
+        _unitOfWorkMock.Setup(u => u.Courses.GetByIdChecked(courseId))
+                       .ThrowsAsync(new InstanceNotFoundException("Course", courseId));
 
-        var result = await _courseService.GetCourseById(courseId);
+        InstanceNotFoundException ex = Assert.ThrowsAsync<InstanceNotFoundException>(async () => await _courseService.GetCourseById(courseId));
 
-        Assert.IsNull(result);
+        Assert.That(ex.Message, Is.EqualTo($"Could not find an instance of 'Course' by id {courseId}"));
     }
 
     [Test]
-    public async Task UpdateCourse_CourseExistsAndValidationPasses_ReturnsTrue()
+    public async Task UpdateCourse_CourseExistsAndValidationPasses_UpdatesCourseDetails()
     {
         var courseId = 1;
         var updateCourseDto = new UpdateCourseDto("new_name", "new_description");
         var course = new Course("name", "description");
 
-        _unitOfWorkMock.Setup(u => u.Courses.GetById(courseId)).ReturnsAsync(course);
+        _unitOfWorkMock.Setup(u => u.Courses.GetByIdChecked(courseId)).ReturnsAsync(course);
         _validationHandlerMock.Setup(v => v.ValidateAsync(updateCourseDto)).Returns(Task.CompletedTask);
+
         _unitOfWorkMock.Setup(u => u.SaveChanges()).ReturnsAsync(1);
 
-        var result = await _courseService.UpdateCourse(courseId, updateCourseDto);
+        await _courseService.UpdateCourse(courseId, updateCourseDto);
 
-        Assert.IsTrue(result);
+        _unitOfWorkMock.Verify(u => u.SaveChanges(), Times.Once);
+
+        Assert.That(course.Name, Is.EqualTo("new_name"));
+        Assert.That(course.Description, Is.EqualTo("new_description"));
     }
 
+
     [Test]
-    public async Task UpdateCourse_CourseDoesNotExist_ReturnsFalse()
+    public void UpdateCourse_CourseDoesNotExist_ThrowsInstanceNotFoundException()
     {
         var courseId = 1;
         var updateCourseDto = new UpdateCourseDto("new_name", "new_description");
 
-        _unitOfWorkMock.Setup(u => u.Courses.GetById(courseId)).ReturnsAsync(default(Course));
+        _unitOfWorkMock.Setup(u => u.Courses.GetByIdChecked(courseId))
+                       .Throws(new InstanceNotFoundException("Course", courseId));
 
-        var result = await _courseService.UpdateCourse(courseId, updateCourseDto);
-
-        Assert.IsFalse(result);
+        Assert.ThrowsAsync<InstanceNotFoundException>(
+            async () => await _courseService.UpdateCourse(courseId, updateCourseDto)
+        );
     }
 
     [Test]
@@ -132,28 +141,49 @@ public class CourseServiceTests
     }
 
     [Test]
-    public async Task DeleteCourse_CourseExists_ReturnsTrue()
+    public async Task DeleteCourse_CourseExists_MethodsAreCalled()
     {
         int courseId = 1;
 
-        _unitOfWorkMock.Setup(u => u.Courses.Delete(courseId)).ReturnsAsync(true);
+        _unitOfWorkMock.Setup(u => u.Courses.DeleteByIdChecked(courseId)).Returns(Task.CompletedTask);
         _unitOfWorkMock.Setup(u => u.SaveChanges()).ReturnsAsync(1);
 
-        var result = await _courseService.DeleteCourse(courseId);
+        await _courseService.DeleteCourse(courseId);
 
-        Assert.IsTrue(result);
+        _unitOfWorkMock.Verify(u => u.Courses.DeleteByIdChecked(courseId), Times.Once);
+        _unitOfWorkMock.Verify(u => u.SaveChanges(), Times.Once);
     }
 
     [Test]
-    public async Task DeleteCourse_CourseDoesNotExist_ReturnsFalse()
+    public void DeleteCourse_CourseDoesNotExist_ThrowsInstanceNotFoundException()
     {
         int courseId = 1;
 
-        _unitOfWorkMock.Setup(u => u.Courses.Delete(courseId)).ReturnsAsync(false);
+        _unitOfWorkMock.Setup(u => u.Courses.DeleteByIdChecked(courseId))
+                                   .Throws(new InstanceNotFoundException("Course", courseId));
 
-        var result = await _courseService.DeleteCourse(courseId);
-
-        Assert.IsFalse(result);
+        Assert.ThrowsAsync<InstanceNotFoundException>(
+                async () => await _courseService.DeleteCourse(courseId));
     }
+
+    [Test]
+    public async Task GetAllCourses_ReturnsCourseDtos()
+    {
+        var courses = new List<Course>
+    {
+        new Course ("name1", "description1"),
+        new Course ("name2", "description2")
+    };
+
+        _unitOfWorkMock.Setup(u => u.Courses.Get(null, null, 0, int.MaxValue, true, It.IsAny<Expression<Func<Course, object>>[]>()))
+                       .ReturnsAsync(courses);
+
+        var result = await _courseService.GetAllCourses();
+
+        Assert.IsNotNull(result);
+        Assert.That(result.Count, Is.EqualTo(courses.Count));
+        Assert.IsTrue(result.All(c => c is CourseDto));
+    }
+
 }
 
