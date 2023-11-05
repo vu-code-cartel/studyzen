9d2740a17e04284735a1fb68a52c39be62689b22
vestacic <vesta.cic@gmail.com>
2023-11-05 23:36:08 +0200

    add registration endpoint

---
+++ server/src/Api/Controllers/ApplicationUserController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using StudyZen.Api.Extensions;
+using StudyZen.Application.Dtos;
+using StudyZen.Application.Services;
+
+namespace StudyZen.Api.Controllers;
+
+[ApiController]
+public sealed class ApplicationUserController : ControllerBase
+{
+    private readonly IApplicationUserService _applicationUserService;
+
+    public ApplicationUserController(IApplicationUserService applicationUserService)
+    {
+        _applicationUserService = applicationUserService;
+    }
+
+    [HttpPost]
+    [Route("register")]
+    public async Task<IActionResult> RegisterApplicationUser([FromBody] RegisterApplicationUserDto request)
+    {
+        request.ThrowIfRequestArgumentNull(nameof(request));
+        var newApplicationUser = await _applicationUserService.CreateApplicationUser(request);
+        return Ok(newApplicationUser);
+    }
+}
