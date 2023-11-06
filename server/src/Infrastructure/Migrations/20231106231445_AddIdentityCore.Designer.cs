f893d2841672f8db8e08ef38e24ef45f310b8cec
vestacic <vesta.cic@gmail.com>
2023-11-07 01:15:16 +0200

    update migrations

--- server/src/Infrastructure/Migrations/20231106231445_AddIdentityCore.Designer.cs
+++ server/src/Infrastructure/Migrations/20231106231445_AddIdentityCore.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StudyZen.Infrastructure.Persistence;
 
 #nullable disable
 
-namespace StudyZen.Infrastructure.Migrations
+namespace StudyZen.Infrastructure.src.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231106231445_AddIdentityCore")]
+    partial class AddIdentityCore
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
