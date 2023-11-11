380c48d93ff227a03e6b6105f025a9fdd6b527a5
EvelinaTemnolonskyte <evelinaemily2@gmail.com>
2023-11-11 18:18:51 +0200

    Update AuditableEntityInterceptor.cs

--- server/src/Infrastructure/Persistence/Interceptors/AuditableEntityInterceptor.cs
+++ server/src/Infrastructure/Persistence/Interceptors/AuditableEntityInterceptor.cs
@@ -1,4 +1,4 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using StudyZen.Domain.Interfaces;
 using StudyZen.Domain.ValueObjects;
@@ -51,50 +51,6 @@ public class AuditableEntityInterceptor : SaveChangesInterceptor
         }
     }
     public void SetUpdateStamp(object instance)
-    {
-        if (instance is IAuditable auditable)
-        {
-            auditable.UpdatedBy = new UserActionStamp(Environment.UserName, DateTime.UtcNow);
-        }
-    }
-}
-using Microsoft.EntityFrameworkCore.Diagnostics;
-using StudyZen.Domain.Interfaces;
-using StudyZen.Domain.ValueObjects;
-
-namespace StudyZen.Infrastructure.Persistence;
-
-public class AuditableEntityInterceptor: SaveChangesInterceptor
-{
-    public override InterceptionResult<int> SavingChanges( DbContextEventData eventData, InterceptionResult<int> result)
-    {
-        var entries = eventData.Context.ChangeTracker.Entries();
-
-        foreach (var entry in entries)
-        {
-            if (entry.State == EntityState.Added)
-            {
-                SetCreateStamp(entry.Entity);
-            }
-            else if (entry.State == EntityState.Modified)
-            {
-                SetUpdateStamp(entry.Entity);
-            }
-        }
-
-        return result;
-    }
-    
-    public static void SetCreateStamp(object instance)
-    {
-        if (instance is IAuditable auditable)
-        {
-            auditable.CreatedBy = new UserActionStamp(Environment.UserName, DateTime.UtcNow);
-            auditable.UpdatedBy = auditable.CreatedBy with { }; // shallow copy
-        }
-    }
-
-    public static void SetUpdateStamp(object instance)
     {
         if (instance is IAuditable auditable)
         {
