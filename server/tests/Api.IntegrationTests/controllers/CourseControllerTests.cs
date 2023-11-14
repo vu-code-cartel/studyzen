1e517113c2694e2e53a17a1d9106dc560211f37a
AkvileR <56289629+AkvileR@users.noreply.github.com>
2023-11-14 18:17:51 +0200

    CourseController tests

--- server/tests/Api.IntegrationTests/controllers/CourseControllerTests.cs
+++ server/tests/Api.IntegrationTests/controllers/CourseControllerTests.cs
@@ -1,29 +1,93 @@
 ﻿using System.Net;
 using System.Net.Http.Json;
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
 using StudyZen.Application.Dtos;
+using StudyZen.Domain.Entities;
 
 namespace Api.IntegrationTests.controllers;
+
 public class CourseControllerTests : IClassFixture<WebApplicationFactory<Program>>
 {
-    private readonly WebApplicationFactory<Program> _factory;
+    private readonly HttpClient _httpClient;
+    private readonly CreateCourseDto _createCourseDto;
+    private readonly UpdateCourseDto _updateCourseDto;
 
     public CourseControllerTests(WebApplicationFactory<Program> factory)
     {
-        _factory = factory;
+        _httpClient = factory.CreateClient();
+        _createCourseDto = new CreateCourseDto("Test name", "Test desc");
+        _updateCourseDto = new UpdateCourseDto("New name", "New desc");
+    }
+
+    [Fact]
+    public async Task CreateCourse()
+    {
+        var response = await _httpClient.PostAsJsonAsync("Courses", _createCourseDto);
+        var course = await response.Content.ReadFromJsonAsync<CourseDto>();
+
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        Assert.NotNull(course);
+        Assert.Equal(_createCourseDto.Name, course.Name);
+        Assert.Equal(_createCourseDto.Description, course.Description);
     }
 
     [Fact]
     public async Task GetCourse()
     {
-        var client = _factory.CreateClient();
-        var courseId = 1;
+        var createResponse = await _httpClient.PostAsJsonAsync("Courses", _createCourseDto);
+        var newCourse = await createResponse.Content.ReadFromJsonAsync<CourseDto>();
+        Assert.NotNull(newCourse);
 
-        var response = await client.GetAsync($"Courses/{courseId}");
+        var response = await _httpClient.GetAsync($"Courses/{newCourse.Id}");
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         var course = await response.Content.ReadFromJsonAsync<CourseDto>();
 
         Assert.NotNull(course);
+        Assert.Equal(newCourse.Name, course.Name);
+        Assert.Equal(newCourse.Description, course.Description);
+    }
+
+    [Fact]
+    public async Task GetAllCourses()
+    {
+        var response = await _httpClient.GetAsync("Courses");
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        var allCourses = await response.Content.ReadFromJsonAsync<IReadOnlyCollection<CourseDto>>();
+        Assert.NotNull(allCourses);
+    }
+
+    [Fact]
+    public async Task UpdateCourse()
+    {
+        var createResponse = await _httpClient.PostAsJsonAsync("Courses", _createCourseDto);
+        var newCourse = await createResponse.Content.ReadFromJsonAsync<CourseDto>();
+        Assert.NotNull(newCourse);
+
+        var response = await _httpClient.PatchAsJsonAsync($"Courses/{newCourse.Id}", _updateCourseDto);
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+        var getResponse = await _httpClient.GetAsync($"Courses/{newCourse.Id}");
+        var course = await getResponse.Content.ReadFromJsonAsync<CourseDto>();
+        Assert.NotNull(course);
+        Assert.Equal(_updateCourseDto.Name, course.Name);
+        Assert.Equal(_updateCourseDto.Description, course.Description);
+    }
+
+    [Fact]
+    public async Task DeleteCourse()
+    {
+        var createResponse = await _httpClient.PostAsJsonAsync("Courses", _createCourseDto);
+        var newCourse = await createResponse.Content.ReadFromJsonAsync<CourseDto>();
+        Assert.NotNull(newCourse);
+
+        var response = await _httpClient.DeleteAsync($"Courses/{newCourse.Id}");
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+        var getResponse = await _httpClient.GetAsync($"Courses/{newCourse.Id}");
+        Assert.Equal(HttpStatusCode.UnprocessableEntity, getResponse.StatusCode);
     }
 }
 
